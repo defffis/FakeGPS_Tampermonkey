2279d0c483ba67a526e7549dc28be7905afd2294
defffis <36509310+defffis@users.noreply.github.com>
2023-08-04 11:53:50 +0300

    Update Menu Fake GPS.js

--- Menu Fake GPS.js
+++ Menu Fake GPS.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Menu Fake GPS
 // @namespace    https://github.com/defffis/FakeGPS_Tampermonkey
-// @version      1.0.5
+// @version      1.1.0
 // @license      AGPLv3
 // @author       defffis
 // @description  Falsify GPS location to protect privacy, or to provide a virtual location sensor device. This script should either be be configured for specific sites only, or for all sites but excludes some specific sites.
@@ -33,76 +33,92 @@
     return hostname;
   }
 
+  // Функция для генерации случайного местоположения
+  function generateRandomLocation() {
+    return {
+      latitude: Math.random() * (53.999999 - 45.000001) + 45.000001,
+    longitude: Math.random() * (30.630999 - 2.000000) + 2.000000,
+    accuracy: Math.random() * (0.170001 - 0.070001) + 0.070001,
+    altitude: Math.floor(Math.random() * (250 - 180 + 1)) + 180,
+    altitudeAccuracy: 1,
+    heading: Math.random() * 360,
+    speed: Math.random() * 56
+    };
+  }
+
   // Проверяем, нужно ли выполнять скрипт на текущем сайте
   function checkMatch() {
-  let currentPageUrl = window.location.href;
-  let currentPageDomain = getDomainFromUrl(currentPageUrl);
-  let existingMatch = GM_getValue('myScriptMatch', '');
+    let currentPageUrl = window.location.href;
+    let currentPageDomain = getDomainFromUrl(currentPageUrl);
+    let existingMatch = GM_getValue('myScriptMatch', '');
 
-  if (existingMatch.includes(currentPageDomain)) {
-    // Выполняем скрипт только на сайтах, добавленных в match
+    if (existingMatch.includes(currentPageDomain)) {
+      // Выполняем скрипт только на сайтах, добавленных в match
 
-    // Получаем сохраненные координаты для текущего сайта
-    let savedLocation = GM_getValue(currentPageDomain, null);
+      // Получаем сохраненные координаты для текущего сайта
+      let savedLocation = GM_getValue(currentPageDomain, null);
 
-    // Если для текущего сайта есть сохраненные координаты, используем их
-    if (savedLocation !== null) {
-      setMockLocation(savedLocation);
+      // Если для текущего сайта есть сохраненные координаты, используем их
+      if (savedLocation !== null) {
+        setMockLocation(savedLocation);
+      } else {
+        // Иначе используем случайное местоположение по умолчанию
+        setMockLocation(generateRandomLocation());
+      }
+
+      // Вместо этого места можно разместить ваш код для выполнения на этих сайтах
+      console.log('Скрипт выполняется на сайте: ' + currentPageDomain);
     } else {
-      // Иначе используем значения по умолчанию
-      setMockLocation({
-        latitude: defaultLatitude,
-        longitude: defaultLongitude,
-        accuracy: defaultAccuracy,
-        altitude: defaultAltitude,
-        altitudeAccuracy: defaultAltitudeAccuracy,
-        heading: defaultHeading,
-        speed: defaultSpeed
-      });
+      // Если сайт нигде не добавлен, используем случайное местоположение по умолчанию
+      setMockLocation(generateRandomLocation());
     }
-
-    // Вместо этого места можно разместить ваш код для выполнения на этих сайтах
-    console.log('Скрипт выполняется на сайте: ' + currentPageDomain);
   }
-}
 
   // Функция для установки фиктивного местоположения
-function setMockLocation(location) {
-  // Create a mock geolocation object
-  const mockGeolocation = {
-    getCurrentPosition: function(succ, err) {
-      setTimeout(() => {
-        let coords = {
-          latitude: location.latitude,
-          longitude: location.longitude,
-          accuracy: location.accuracy,
-          altitude: location.altitude,
-          altitudeAccuracy: location.altitudeAccuracy,
-          heading: location.heading,
-          speed: location.speed
-        };
-        let timestamp = (new Date()).getTime();
-        succ({ coords, timestamp });
-      }, 0);
-    },
-    watchPosition: function(succ, err) {
-      return setInterval(() => {
-        this.getCurrentPosition(succ, err);
-      }, 1000); // Set the watchInterval to 1000ms (1 second)
-    },
-    clearWatch: function(id) {
-      clearInterval(id);
-    }
-  };
+  function setMockLocation(location) {
+    // Проверяем, присутствует ли текущий домен в списке исключений
+    let currentPageUrl = window.location.href;
+    let currentPageDomain = getDomainFromUrl(currentPageUrl);
+    let exclusions = GM_getValue('myScriptExclusions', '');
 
-  // Override the original navigator.geolocation with the mock object
-  Object.defineProperty(navigator, 'geolocation', {
-    value: mockGeolocation
-  });
-}
+    if (!exclusions.includes(currentPageDomain)) {
+      // Create a mock geolocation object
+      const mockGeolocation = {
+        getCurrentPosition: function(succ, err) {
+          setTimeout(() => {
+            let coords = {
+              latitude: location.latitude,
+              longitude: location.longitude,
+              accuracy: location.accuracy,
+              altitude: location.altitude,
+              altitudeAccuracy: location.altitudeAccuracy,
+              heading: location.heading,
+              speed: location.speed
+            };
+            let timestamp = (new Date()).getTime();
+            succ({ coords, timestamp });
+          }, 0);
+        },
+        watchPosition: function(succ, err) {
+          return setInterval(() => {
+            this.getCurrentPosition(succ, err);
+          }, 1000); // Set the watchInterval to 1000ms (1 second)
+        },
+        clearWatch: function(id) {
+          clearInterval(id);
+        }
+      };
+
+      // Override the original navigator.geolocation with the mock object
+      Object.defineProperty(navigator, 'geolocation', {
+        value: mockGeolocation
+      });
+    }
+  }
 
   // Функция для добавления текущей страницы в match скрипта
   function addCurrentPageToMatch() {
+      removeCurrentSiteFromStorage;
     let currentPageUrl = window.location.href;
     let currentPageDomain = getDomainFromUrl(currentPageUrl);
     let existingMatch = GM_getValue('myScriptMatch', '');
@@ -110,23 +126,64 @@ function setMockLocation(location) {
     if (!existingMatch.includes(currentPageDomain)) {
       let newMatch = existingMatch ? existingMatch + ',' + currentPageDomain : currentPageDomain;
       GM_setValue('myScriptMatch', newMatch);
-      alert('Домен текущей страницы добавлен в хранилище скрипта.');
+      //alert('Домен текущей страницы добавлен в хранилище скрипта.');
     } else {
-      alert('Домен текущей страницы уже присутствует в хранилище скрипта.');
+      //alert('Домен текущей страницы уже присутствует в хранилище скрипта.');
     }
   }
 
+  // Функция для добавления текущей страницы в список исключений
+  function addCurrentPageToExclusions() {
+      removeCurrentSiteFromStorage;
+    let currentPageUrl = window.location.href;
+    let currentPageDomain = getDomainFromUrl(currentPageUrl);
+    let existingExclusions = GM_getValue('myScriptExclusions', '');
 
-    // Функция для удаления текущего сайта из хранилища
+    if (!existingExclusions.includes(currentPageDomain)) {
+      let newExclusions = existingExclusions ? existingExclusions + ',' + currentPageDomain : currentPageDomain;
+      GM_setValue('myScriptExclusions', newExclusions);
+      alert('Домен текущей страницы добавлен в список исключений. Теперь для него не будет использоваться фиктивное местоположение.');
+      // Удаляем текущий домен из списка myScriptMatch, если он там есть
+      let existingMatch = GM_getValue('myScriptMatch', '');
+      if (existingMatch.includes(currentPageDomain)) {
+        existingMatch = existingMatch.replace(currentPageDomain, '');
+        existingMatch = existingMatch.replace(',,', ','); // Удаляем лишние запятые, если они возникли после удаления
+        if (existingMatch.startsWith(',')) {
+          existingMatch = existingMatch.substring(1); // Удаляем запятую в начале, если она возникла после удаления
+        }
+        GM_setValue('myScriptMatch', existingMatch);
+      }
+    } else {
+      alert('Домен текущей страницы уже присутствует в списке исключений.');
+    }
+  }
+
+  // Функция для удаления текущего сайта из хранилища
 function removeCurrentSiteFromStorage() {
   let currentPageUrl = window.location.href;
   let currentPageDomain = getDomainFromUrl(currentPageUrl);
+
+  // Удаляем текущий домен из списка myScriptMatch, если он там есть
+  let existingMatch = GM_getValue('myScriptMatch', '');
+  let matchArray = existingMatch.split(',');
+  matchArray = matchArray.filter(domain => domain !== currentPageDomain);
+  GM_setValue('myScriptMatch', matchArray.join(','));
+
+  // Удаляем текущий домен из списка myScriptExclusions, если он там есть
+  let existingExclusions = GM_getValue('myScriptExclusions', '');
+  let exclusionArray = existingExclusions.split(',');
+  exclusionArray = exclusionArray.filter(domain => domain !== currentPageDomain);
+  GM_setValue('myScriptExclusions', exclusionArray.join(','));
+
   GM_deleteValue(currentPageDomain);
-  alert('Местоположение для текущего сайта удалено из хранилища.');
+  //alert('Местоположение для текущего сайта удалено из хранилища.');
 }
+
 
   // Функция для сохранения выбранного местоположения для текущего сайта
   function saveLocationForCurrentSite(location) {
+    removeCurrentSiteFromStorage();
+    addCurrentPageToMatch();
     let currentPageUrl = window.location.href;
     let currentPageDomain = getDomainFromUrl(currentPageUrl);
     GM_setValue(currentPageDomain, location);
@@ -135,37 +192,37 @@ function removeCurrentSiteFromStorage() {
 
   // Список готовых пресетов геопозиций
   var presets = {
-      "Минск Песочница": {
-      latitude: 53.915523,
-      longitude: 27.568876,
-      accuracy: 0.000001,
+    "Минск Песочница": {
+      latitude: 53.915525,
+      longitude: 27.568870,
+      accuracy: 0.009469,
       altitude: 214,
       altitudeAccuracy: 1,
       heading: 135,
       speed: 0
     },
-      "Минск Троицкого/Сторожовская": {
-      latitude: 53.91172,
-      longitude: 27.55664,
-      accuracy: 0.000001,
+    "Минск Троицкого/Сторожовская": {
+      latitude: 53.912972,
+      longitude: 27.555453,
+      accuracy: 0.009469,
       altitude: 214,
       altitudeAccuracy: 1,
       heading: 135,
       speed: 0
     },
-      "Минск Галерея": {
-      latitude: 53.90851,
-      longitude: 27.54856,
-      accuracy: 0.000001,
+    "Минск Галерея": {
+      latitude: 53.908512,
+      longitude: 27.548552,
+      accuracy: 0.009469,
       altitude: 214,
       altitudeAccuracy: 1,
       heading: 135,
       speed: 0
     },
-      "Минск Мир": {
-      latitude: 53.86920,
-      longitude: 27.53554,
-      accuracy: 0.000001,
+    "Минск Мир": {
+      latitude: 53.869199,
+      longitude: 27.535535,
+      accuracy: 0.009469,
       altitude: 214,
       altitudeAccuracy: 1,
       heading: 135,
@@ -174,7 +231,7 @@ function removeCurrentSiteFromStorage() {
     "Минск Мир 2": {
       latitude: 53.867464,
       longitude: 27.541219,
-      accuracy: 0.000001,
+      accuracy: 0.009469,
       altitude: 214,
       altitudeAccuracy: 1,
       heading: 135,
@@ -183,7 +240,7 @@ function removeCurrentSiteFromStorage() {
     "ЖК парк Челюскинцев": {
       latitude: 53.923994,
       longitude: 27.624810,
-      accuracy: 0.000001,
+      accuracy: 0.009469,
       altitude: 214,
       altitudeAccuracy: 1,
       heading: 135,
@@ -193,11 +250,13 @@ function removeCurrentSiteFromStorage() {
   };
 
   // Добавляем кнопку в меню Tampermonkey для добавления текущей страницы в match скрипта
-  GM_registerMenuCommand('Добавить текущую страницу в правила', addCurrentPageToMatch);
+  //GM_registerMenuCommand('Добавить текущую страницу в правила', addCurrentPageToMatch);
 
+  // Добавляем кнопку в меню Tampermonkey для добавления текущего сайта в список исключений
+  GM_registerMenuCommand('Использовать реальное местоположение', addCurrentPageToExclusions);
 
-    // Добавляем кнопку в меню Tampermonkey для удаления текущего сайта из хранилища
-GM_registerMenuCommand('Удалить местоположение', removeCurrentSiteFromStorage);
+  // Добавляем кнопку в меню Tampermonkey для удаления текущего сайта из хранилища
+  GM_registerMenuCommand('Использовать рандомные координаты', removeCurrentSiteFromStorage);
 
   // Добавляем список пресетов геопозиций в меню Tampermonkey
   Object.keys(presets).forEach(presetName => {
